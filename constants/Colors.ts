df07b415b998cde8e1f118c88c0178f9892724fe
Matheus Oliveira <oliveiramarinho789@gmail.com>
2024-06-26 21:34:51 -0400

    (Style): Layout - Alterado cores de dark theme para light theme.

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,26 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#151718'
+const tintColorDark = '#151718'
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#CCCCCC',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    tabIconSelected: tintColorLight
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    /* text: '#ECEDEE', */
+    text: '#11181C',
+    /* background: '#151718', */
+    background: '#CCCCCC',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+    tabIconSelected: tintColorDark
+  }
+}
